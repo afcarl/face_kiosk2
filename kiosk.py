19326dc471cc79f27eca7029e09b6cd078b72b0a
Tambet Matiisen <tambet.matiisen@gmail.com>
2017-11-02 14:32:43 +0200

    Descriptor is reset when the person changes. Added FPS support and text antialiasing.

--- kiosk.py
+++ kiosk.py
@@ -3,7 +3,7 @@ import argparse
 import numpy as np
 import csv
 from sklearn.neighbors import NearestNeighbors
-from imutils.video import VideoStream
+from imutils.video import VideoStream, FPS
 import dlib
 import cv2
 import os
@@ -12,6 +12,8 @@ import sys
 parser = argparse.ArgumentParser()
 parser.add_argument("--capture_device", type=int, default=0)
 parser.add_argument("--delay", type=int, default=1)
+
+# display params
 parser.add_argument("--canvas_width", type=int, default=1024)
 parser.add_argument("--canvas_height", type=int, default=768)
 parser.add_argument("--face_size", type=int, default=160)
@@ -22,16 +24,23 @@ parser.add_argument("--font_thickness", type=int, default=1)
 parser.add_argument("--show_name", type=int, default=1)
 parser.add_argument("--show_distance", type=int, default=1)
 parser.add_argument('--fullscreen', action='store_true', default=False)
+parser.add_argument('--show_fps', action='store_true', default=False)
 
-parser.add_argument("--images_path", default="/storage/LFW/RFLFW")
-parser.add_argument("--data_file", default="RFLFW2.csv")
-parser.add_argument("--vecs_file", default="RFLFW2.npy")
+# face detection params
 parser.add_argument("--detector", choices=['hog', 'cnn', 'opencv', 'cv2gpu'], default='hog')
 parser.add_argument("--cnn_model_path", default='mmod_human_face_detector.dat')
 parser.add_argument("--shape_predictor_path", default='shape_predictor_68_face_landmarks.dat')
-parser.add_argument("--face_rec_model_path", default='dlib_face_recognition_resnet_model_v1.dat')
 parser.add_argument("--upscale", type=int, default=0)
 parser.add_argument("--jitter", type=int, default=0)
+parser.add_argument("--opencv_scalefactor", type=float, default=1.1)
+parser.add_argument("--opencv_minneighbors", type=int, default=5)
+parser.add_argument("--opencv_minsize", type=int, default=80)
+
+# face recognition params
+parser.add_argument("--face_rec_model_path", default='dlib_face_recognition_resnet_model_v1.dat')
+parser.add_argument("--images_path", default="/storage/LFW/RFLFW")
+parser.add_argument("--data_file", default="RFLFW2.csv")
+parser.add_argument("--vecs_file", default="RFLFW2.npy")
 parser.add_argument("--n_neighbors", type=int, default=5)
 parser.add_argument("--algorithm", choices=['auto', 'ball_tree', 'kd_tree', 'brute'], default='auto')
 parser.add_argument("--alpha", type=float, default=0.1)
@@ -67,11 +76,8 @@ with open(args.data_file) as csvfile:
     data = list(reader)
 
 print("Initializing video capture...")
-#video = cv2.VideoCapture(args.capture_device)
 video = VideoStream(args.capture_device).start()
 
-#frame_width = int(video.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH))
-#frame_height = int(video.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT))
 frame = video.read()
 frame_height, frame_width, _ = frame.shape
 
@@ -79,9 +85,8 @@ if args.fullscreen:
     cv2.namedWindow("Video", cv2.WINDOW_NORMAL)
     cv2.setWindowProperty("Video", cv2.WND_PROP_FULLSCREEN, cv2.cv.CV_WINDOW_FULLSCREEN)
 
-# initialize canvas and make background white
+# initialize canvas and make background black
 canvas = np.zeros((args.canvas_height, args.canvas_width, 3), dtype=np.uint8)
-#canvas.fill(255)
 
 # calculate text height
 ((text_width, text_height), retval) = cv2.getTextSize("Abrakadabra", args.font_face, args.font_scale, args.font_thickness)
@@ -93,19 +98,19 @@ padding = int((args.canvas_height - frame_height - args.face_size - text_height)
 assert padding > 0, "Video frame too big, increase canvas height"
 frame_top = 2 * padding + args.face_size + text_height
 
-print(frame_top, frame_height, frame_top + frame_height, canvas.shape[0])
-
 face_gap = int((args.canvas_width - args.face_count * args.face_size) / (args.face_count + 1))
 assert face_gap > 0, "Lower face size"
 face_top = padding
 
 text_top = int(face_top + args.face_size + text_height + 5)
 
+if args.show_fps:
+  fps = FPS().start()
+  last_fps = 0
+
 descriptor = None
 while True:
   # capture frame
-  #ret, frame = video.read()
-  #assert ret, "Frame could not be read from video capture device"
   frame = video.read()
 
   # detect faces
@@ -113,9 +118,9 @@ while True:
   if args.detector == 'opencv':
     rects = detector.detectMultiScale(
         gray,
-        scaleFactor=1.1,
-        minNeighbors=5,
-        minSize=(100, 100),
+        scaleFactor=args.opencv_scalefactor,
+        minNeighbors=args.opencv_minneighbors,
+        minSize=(args.opencv_minsize, args.opencv_minsize),
         flags = cv2.cv.CV_HAAR_SCALE_IMAGE
     )
   elif args.detector == 'cv2gpu':
@@ -145,10 +150,12 @@ while True:
 
     # extract features of the face
     landmarks = predictor(gray, rect)
-    if descriptor is None:
-        descriptor = np.array(facerec.compute_face_descriptor(frame, landmarks, args.jitter))
+    new_descriptor = np.array(facerec.compute_face_descriptor(frame, landmarks, args.jitter))
+    # if there was no previous person, or the person has changed
+    if descriptor is None or np.linalg.norm(descriptor - new_descriptor) > 0.6:
+        descriptor = new_descriptor
+    # if the same person, then average
     else:
-        new_descriptor = np.array(facerec.compute_face_descriptor(frame, landmarks, args.jitter))
         descriptor = (1 - args.alpha) * descriptor + args.alpha * new_descriptor
 
     # find nearest images
@@ -175,14 +182,25 @@ while True:
           ((text_width, text_height), retval) = cv2.getTextSize(text, args.font_face, args.font_scale, args.font_thickness)
           assert retval, "Unable to determine text height"
           text_left = int(face_left + args.face_size / 2 - text_width / 2)
-          cv2.putText(canvas, text, (text_left, text_top), args.font_face, args.font_scale, (240, 240, 240), args.font_thickness)
+          cv2.putText(canvas, text, (text_left, text_top), args.font_face, args.font_scale, (240, 240, 240), args.font_thickness, lineType=cv2.cv.CV_AA)
 
         if args.show_distance:
           text = str(d)
           ((text_width, text_height), retval) = cv2.getTextSize(text, args.font_face, args.font_scale, args.font_thickness)
           assert retval, "Unable to determine text height"
           text_left = int(face_left + args.face_size / 2 - text_width / 2)
-          cv2.putText(canvas, text, (text_left, text_top + text_height + 10), args.font_face, args.font_scale, (240, 240, 240), args.font_thickness)
+          cv2.putText(canvas, text, (text_left, text_top + text_height + 10), args.font_face, args.font_scale, (240, 240, 240), args.font_thickness, lineType=cv2.cv.CV_AA)
+
+  if args.show_fps:
+    fps.update()
+    if fps._numFrames == 10:
+      fps.stop()
+      last_fps = fps.fps()
+      fps = FPS().start()
+    text = "FPS: %.2f" % last_fps
+    ((text_width, text_height), retval) = cv2.getTextSize(text, args.font_face, args.font_scale, args.font_thickness)
+    canvas[args.canvas_height - text_height - 10:, args.canvas_width - text_width - 10:].fill(0)
+    cv2.putText(canvas, text, (args.canvas_width - text_width - 10, args.canvas_height - 10), args.font_face, args.font_scale, (240, 240, 240), args.font_thickness, lineType=cv2.cv.CV_AA)
 
   # display the canvas
   cv2.imshow('Video', canvas)
@@ -191,6 +209,5 @@ while True:
     break;
 
 # when everything is done, release the capture
-#video.release()
 video.stop()
 cv2.destroyAllWindows()
